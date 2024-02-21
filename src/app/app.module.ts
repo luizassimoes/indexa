e987e5ef9af96ca94aa6536cfebd5efbdb9698ae
Luiza Souza Simoes <luiza.simoes@qualidados.com.br>
2024-02-21 18:21:19 -0300

    m2, a3: montando o cabecalho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './componentes/container/container.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContainerComponent
+    ContainerComponent,
+    CabecalhoComponent
   ],
   imports: [
     BrowserModule,
