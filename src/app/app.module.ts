71fc86cacebf911b26a0c4277f2710df659f6b33
Luiza Souza Simoes <luiza.simoes@qualidados.com.br>
2024-02-22 09:46:49 -0300

    m2, a6: criando os separadores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './componentes/container/container.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { SeparadorComponent } from './componentes/separador/separador.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
-    CabecalhoComponent
+    CabecalhoComponent,
+    SeparadorComponent
   ],
   imports: [
     BrowserModule,
