e999215015b8e3a6cffae04ce93b1bdf351bd609
Luiza Souza Simoes <luiza.simoes@qualidados.com.br>
2024-02-22 15:17:22 -0300

    m5, a4: input e ngModel + cobinando os filtros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { ContainerComponent } from './componentes/container/container.component'
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
 import { ContatoComponent } from './componentes/contato/contato.component';
+import { FormsModule } from '@angular/forms';
 
 interface Contato {
   id: number,
@@ -22,7 +23,8 @@ import agenda from './agenda.json'
     ContainerComponent,
     CabecalhoComponent,
     SeparadorComponent,
-    ContatoComponent
+    ContatoComponent,
+    FormsModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -31,8 +33,19 @@ export class AppComponent {
   alfabeto: string[] = 'abcdefghijklmnopqrstuvwxyz'.split('');
   contatos: Contato[] = agenda;
 
+  filtroPorTexto: string = ''
+
+  filtrarContatosPorTexto(): Contato[] {  // ! significa o NOT em JavaScript
+    if (!this.filtroPorTexto) {
+      return this.contatos
+    }
+    return this.contatos.filter(contato => {
+      return contato.nome.toLowerCase().includes(this.filtroPorTexto.toLowerCase())
+    })
+  }
+
   filtrarContatosPorLetraInicial(letra: string) : Contato[] {
-    return this.contatos.filter( contato => {
+    return this.filtrarContatosPorTexto().filter( contato => {
       return contato.nome.toLowerCase().startsWith(letra)
     } )
   }
