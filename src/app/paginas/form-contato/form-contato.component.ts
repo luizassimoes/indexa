3ece2da2886d1da7157bafda52284ebe5e72063b
Luiza Souza Simoes <luiza.simoes@qualidados.com.br>
2024-02-26 08:21:32 -0300

    adicionando Validators

--- src/app/paginas/form-contato/form-contato.component.ts
+++ src/app/paginas/form-contato/form-contato.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { SeparadorComponent } from '../../componentes/separador/separador.component';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-contato',
@@ -22,9 +22,9 @@ export class FormularioContatoComponent {
 
   constructor() {
     this.contatoForm = new FormGroup({
-      nome: new FormControl('Luiza'),
-      telefone: new FormControl('88118822'),
-      email: new FormControl(''),
+      nome: new FormControl('', Validators.required),
+      telefone: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
       aniversario: new FormControl(''),
       redes: new FormControl(''),
       observacoes: new FormControl('')
@@ -32,7 +32,9 @@ export class FormularioContatoComponent {
   }
 
   salvarContato() {
-    console.log(this.contatoForm.value);
+    if(this.contatoForm.valid) {
+      console.log(this.contatoForm.value);
+    }
   }
 
   cancelar() {
