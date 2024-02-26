9d1b5d907ec57223f46f5a0a4791319ec6685fd2
Luiza Souza Simoes <luiza.simoes@qualidados.com.br>
2024-02-26 17:03:23 -0300

    adicionando rotas ao app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { FormularioContatoComponent } from './paginas/form-contato/form-contato.component';
+import { ListContatoComponent } from './paginas/list-contato/list-contato.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'formulario',
+    component: FormularioContatoComponent
+  },
+  {
+    path: 'lista-contatos',
+    component: ListContatoComponent
+  },
+  {
+    path: '',
+    redirectTo: '/lista-contatos',
+    pathMatch: 'full'
+  }
+];
